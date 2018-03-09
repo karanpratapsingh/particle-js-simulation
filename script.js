fa374a65475c472218f3b5e4988f64df5eb8be6c
Karan Pratap Singh <synester.gates4@gmail.com>
2018-03-09 22:40:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Loads Particles from particles.json configration
+
+particlesJS.load('particles-js',
+                 'particles.json',
+                  () => {
+                         console.log("Particles.json init complete.");
+                  });
